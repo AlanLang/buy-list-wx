d97eb5d7ea222f4f9dbfe02e38cbf03aec178dd9
Alan <alan.lang@sliontek.com>
2018-06-20 17:39:59 +0800

    完成购物审批提交的功能

--- pages/buyCommit/buyCommit.js
+++ pages/buyCommit/buyCommit.js
@@ -1,23 +1,44 @@
 const app = getApp()
 Page({
   data: {
-    user:''
+    arrType: [],
+    arrLevel: [],
+    arrPerson:[],
+    user:'',
+    formData:{},
+    typeIndex:0,
+    levelIndex:0,
+    personIndex:0,
+    BuyTime:''
+  },bindTypeChange:function(e){
+    console.log(e)
+    this.setData({
+        typeIndex:e.detail.value
+    })
+  },bindLevelChange:function(e){
+    this.setData({
+      levelIndex:e.detail.value
+    })
+  },bindDateChange:function(e){
+    this.setData({
+      BuyTime:e.detail.value
+    })
   },
   commentSubmit:function(opt){
     let that = this
     let user = app.globalData.userInfo.nickName == 'Åℓαñ✐郎' ? "langwenda" : app.globalData.userInfo.nickName
-    opt.BuyCheckPerson = user
+    let data = opt.detail.value
+    data.BuyAuthor = user
 
     wx.showToast({
         title: "加载中",
         icon: "loading",
         duration: 10000
     })
-
     wx.request({
         url: 'https://www.langwenda.cn/api/BuyCommit',
-        data: opt,
-        method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT  
+        data: data,
+        method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT  
         success: function (res) {
           console.log(res)
           if (res.data.code == 0){
@@ -43,7 +64,12 @@ Page({
           wx.hideToast()
         },
         fail: function () {
-          // fail  
+          wx.showModal({
+            title: "提示",
+            content: "无法连接到服务器",
+            showCancel: false,
+            confirmText: "确定"
+          })
         },
         complete: function () {
           // complete  
@@ -52,6 +78,79 @@ Page({
   },
   onLoad:function(){
     let that = this
+    let user = app.globalData.userInfo.nickName == 'Åℓαñ✐郎' ? "langwenda" : app.globalData.userInfo.nickName
+    wx.request({
+      url: 'https://www.langwenda.cn/api/GetTypes',
+      data:{imei:0},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT  
+      success: function (res) {
+        console.log(res)
+        if (res.data.code == 0){
+          that.setData({
+            arrType : res.data.data
+          })
+        }else{
+          wx.showModal({
+            title: "提示",
+            content: res.data.err,
+            showCancel: false,
+            confirmText: "确定"
+          })
+        }
+        wx.hideToast()
+      },
+      complete: function () {
+        // complete  
+      }
+    })
+    wx.request({
+      url: 'https://www.langwenda.cn/api/GetLevels',
+      data:{imei:0},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT  
+      success: function (res) {
+        console.log(res)
+        if (res.data.code == 0){
+          that.setData({
+            arrLevel : res.data.data
+          })
+        }else{
+          wx.showModal({
+            title: "提示",
+            content: res.data.err,
+            showCancel: false,
+            confirmText: "确定"
+          })
+        }
+        wx.hideToast()
+      },
+      complete: function () {
+        // complete  
+      }
+    })
+    wx.request({
+      url: 'https://www.langwenda.cn/api/GetPersons',
+      data:{user:user,imei:0},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT  
+      success: function (res) {
+        console.log(res)
+        if (res.data.code == 0){
+          that.setData({
+            arrPerson : res.data.data
+          })
+        }else{
+          wx.showModal({
+            title: "提示",
+            content: res.data.err,
+            showCancel: false,
+            confirmText: "确定"
+          })
+        }
+        wx.hideToast()
+      },
+      complete: function () {
+        // complete  
+      }
+    })
   }
 })
 
